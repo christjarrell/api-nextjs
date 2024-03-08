f8f1f7108deed8f0e4f3d039bdcf10b9ed749f1a
Christian Jarrell <christopherjrrll@gmail.com>
2024-03-08 08:10:57 +0000

    first commit

---
+++ src/app/dashboard/rest-todo/page.tsx
@@ -0,0 +1,25 @@
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
+import prisma from "@/app/lib/prisma";
+import { NewTodo, TodosGrid } from "@/todos";
+
+
+export const metadata = {
+  title: 'Listado de TODOs',
+  description: 'Listado de TODOs',
+};
+
+export default async function RestTodosPage() {
+
+  const todos = await prisma.todo.findMany({ orderBy: { description: 'asc' } });
+
+  return (
+    <div>
+      <div className="w-full px-3 mx-5 mb-5">
+        <NewTodo />
+      </div>
+      <TodosGrid todos={todos} />
+    </div>
+  );
+}
